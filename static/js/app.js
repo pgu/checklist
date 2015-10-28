37bf9d7836e2dc9af5faa7f6ae73b4775a6e0b2e
pgu <guilcher.pascal@gmail.com>
2015-10-28 14:39:55 +0100

    WIP:

--- static/js/app.js
+++ static/js/app.js
@@ -4,11 +4,7 @@
   angular.module('checklistApp', [ 'ngMaterial', 'ngMessages' ]);
 
   angular.module('checklistApp')
-    .config(function ($mdThemingProvider, $mdIconProvider) {
-
-      $mdIconProvider
-        .icon('save', './bower_components/material-design-icons/content/svg/production/ic_save_24px.svg', 24)
-      ;
+    .config(function ($mdThemingProvider) {
 
       $mdThemingProvider.theme('default')
         .primaryPalette('blue')
